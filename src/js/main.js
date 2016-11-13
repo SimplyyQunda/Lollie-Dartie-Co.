1cfb0254198b17d87c6a4a5da866bffd3be0b83c
DaQunda Magwood <dmagwood13@gmail.com>
2016-11-13 13:52:12 -0500

    All elements are in place.

--- src/js/main.js
+++ src/js/main.js
@@ -13,12 +13,28 @@ function getproductInfo () {
 productInfo().then(getdesc);
 
 $('#googleMaps').append(getMap);
+// $('#dartsImg').append(rotateImg);
 
 companyLogo().then(getLogos);
-userProfile().then(profile);
-userProfile().then(profile);
-userProfile().then(profile);
 testmonInfo().then(testmInfo);
 testmonInfo().then(infoName);
+userProfile().then(profile);
+userProfile().then(profile);
+userProfile().then(profile);
 
 
+
+//Functions activates my images to rotate automatly
+
+function rotateImg () {
+	var current = $('#dartsImg .active');
+	var next = ($('#dartsImg .active').next().length > 0)
+				? $('#dartsImg .active').next() : 
+				$('#dartsImg img:first');
+			$acive.removeClass('active');
+			$next.fadeIn().addClass('active');		
+};
+
+	$(document).ready(function(){
+		setInterval('rotateImg()', 5000);
+	});
--- src/js/template.js
+++ src/js/template.js
@@ -6,7 +6,15 @@ function getdesc (data) {
 
 	var dataHTML = `
 	 <div class="title"> ${data.data.product.title} </div>
-	 <div class="script"> ${data.data.product.description} </div>`;
+	 <div class="script"> ${data.data.product.description} </div>
+	 <div id="media">
+	  <button class="cart">Add to Cart</button>
+      <a href="http://twitter.com/"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+      <a href="http://facebook.com/"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
+      <a href="http://pinterest.com/"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
+      <a href="http://instagram.com/"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+    </div>
+	 `;
 	 $('#infoAPI').append(dataHTML);
 };
 
@@ -16,7 +24,7 @@ function getLogos (data) {
 		// console.log(data);
 		var companyImg = data.image_url;
 		// console.log(companyImg);
-		var companyHTML = `<img src="${companyImg}"/>`;
+		var companyHTML = `<img class="Comlogo" src="${companyImg}"/>`;
 		$('.companyLt').append(companyHTML);
 	});
 };
@@ -25,7 +33,7 @@ function profile (data) {
 
 		data.results.map(function (data) {
 		var userPic = data.picture.large;
-		var userHTML = `<img src="${userPic}"/>`;
+		var userHTML = `<img class="profileUser" src="${userPic}"/>`;
 		$('.testimony').append(userHTML);
 	});
 };
@@ -35,9 +43,11 @@ function testmInfo (data) {
 		var infoTesti = data.review;
 		var infoName = data.name;
 
-		var testiHTML = `<div> ${infoTesti} </div>
-						 <div>  ${infoName} </div>`
-		$('.testimony').append(testiHTML);
+		var testiHTML = `<div>
+						 <div> ${infoName}  </div>
+						 <div> ${infoTesti} </div>
+						 </div>`
+		$('.testimonyInfo').append(testiHTML);
 	})
 }
 
