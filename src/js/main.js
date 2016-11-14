7b0fa39082cb58f04f3181e72745604a1867f2e1
DaQunda Magwood <dmagwood13@gmail.com>
2016-11-13 19:51:47 -0500

    Image gallery is rotating automatically

--- src/js/main.js
+++ src/js/main.js
@@ -27,14 +27,16 @@ userProfile().then(profile);
 //Functions activates my images to rotate automatly
 
 function rotateImg () {
-	var current = $('#dartsImg .active');
-	var next = ($('#dartsImg .active').next().length > 0)
-				? $('#dartsImg .active').next() : 
-				$('#dartsImg img:first');
-			$acive.removeClass('active');
-			$next.fadeIn().addClass('active');		
-};
+	var current = $('#slideTime .active');
+	var next = ($('#slideTime .active').next().length > 0)
+				? $('#slideTime .active').next() : 
+				$('#slideTime li:first');
+			current.fadeOut(function(){
+				current.removeClass('active');
+				next.fadeIn().addClass('active');
+			});	
+		}
 
 	$(document).ready(function(){
-		setInterval('rotateImg()', 5000);
+		setInterval(rotateImg, 3000);
 	});
